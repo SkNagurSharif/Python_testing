a4ac451f99a96d06997e41e30bd18855eb0528b9
Shaik Nagur Sharif <sknagursharif@outlook.com>
2023-11-03 09:55:36 +0530

    just updated test_rectangle.py

--- pytest_learn/test/test_rectangle.py
+++ pytest_learn/test/test_rectangle.py
@@ -1,6 +1,7 @@
 import pytest
 import dev.Shape as Shape
 
+# with fixture
 @pytest.fixture
 
 def Test_Rectangle():
@@ -12,3 +13,12 @@ def test_rect_area(Test_Rectangle):
 def test_rect_peri(Test_Rectangle):
     assert Test_Rectangle.perimeter() == (2*20)*(2*30)
         
+# Without fixture        
+
+def test_rect_Area():
+    rectangle = Shape.Rectangle(20,30)
+    assert rectangle.area() == 20*30
+    
+def test_rect_Peri(Test_Rectangle):
+    rectangle = Shape.Rectangle(20,30)
+    assert rectangle.perimeter() == (2*20)*(2*30)
